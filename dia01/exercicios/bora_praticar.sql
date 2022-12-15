e703d41ebc650f7582c579cabcea8849bb0cd407
hugonyland <hugonyland>
2022-12-15 00:02:19 +0000

    Exercícios 1 a 4

---
+++ dia01/exercicios/bora_praticar.sql
@@ -0,0 +1,43 @@
+-- Databricks notebook source
+-- DBTITLE 1,Exercício 01
+-- 01 Selecione todos os clientes paulistanos
+
+SELECT *
+
+FROM silver_olist.cliente
+WHERE descCidade = 'sao paulo'
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Exercício 02
+-- 02 Selecione todos os clientes paulistas
+
+SELECT *
+
+FROM silver_olist.cliente
+WHERE descUF = 'SP'
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Exercício 03
+-- 03 Selecione todos os vendedores cariocas e paulistas
+
+SELECT *
+
+FROM silver_olist.vendedor
+
+WHERE descUF = 'SP' 
+OR descCidade = 'rio de janeiro'
+
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Exercício 04
+-- 04 Selecione todos os produtos de perfumaria e bebês com altura maior que 5cm.
+
+SELECT *
+
+FROM silver_olist.produto
+
+WHERE descCategoria IN ('perfumaria', 'bebes')
+AND vlAlturaCm > 5
